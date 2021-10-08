ef138b4e6de945988219c16a50007131ce4aa417
djzhang <trujunzhang@gmail.com>
2021-10-08 13:35:13 +0800

    flutter plugin.

---
+++ lib/my_plugin.dart
@@ -0,0 +1,9 @@
+library my_plugin;
+
+///
+/// https://github.com/simplezhli/flutter_deer
+export 'src/flutter_deer/util/index.dart';
+
+export 'src/components/index.dart';
+
+export 'src/services/theme_service.dart';
