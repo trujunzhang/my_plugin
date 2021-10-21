6dd64175bf9b83a3911e93afc51570fbf4b72b04
djzhang <trujunzhang@gmail.com>
2021-10-21 09:07:47 +0800

    gradient.

--- lib/src/effect/app_gradient.dart
+++ lib/src/effect/app_gradient.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppGradient {
-  static linearGradient(Color color) {
-    LinearGradient(
+  // Example:
+  // Container(
+  //    decoration: BoxDecoration(
+  //         gradient: AppGradient.linearGradient(const Color(0xFF151C26))))
+  static Gradient linearGradient(Color color) {
+    return LinearGradient(
         begin: Alignment.bottomCenter,
         end: Alignment.topCenter,
-        stops: const [
-          0.0,
-          0.9
-        ],
-        colors: [
-          color.withOpacity(1.0),
-          color.withOpacity(0.0)
-        ]);
+        stops: const [0.0, 0.9],
+        colors: [color.withOpacity(1.0), color.withOpacity(0.0)]);
   }
 }
