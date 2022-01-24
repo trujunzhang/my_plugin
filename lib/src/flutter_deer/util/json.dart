9b2858fedaf406e26b341c1c68a7c8b4b2c7262b
djzhang <trujunzhang@gmail.com>
2022-01-24 14:10:59 +0800

    load Json.

--- lib/src/flutter_deer/util/json.dart
+++ lib/src/flutter_deer/util/json.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 
-loadJson(String asset) async {
+Future<Map<String, dynamic>> loadJson(String asset) async {
   String data = await rootBundle.loadString(asset);
   return json.decode(data);
 }
